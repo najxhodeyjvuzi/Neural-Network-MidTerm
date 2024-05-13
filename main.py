26d8ff4b70a11cb84d1ed0380526a525432caac6
najxhodeyjvuzi <najxhodeyjvuzi@proton.me>
2024-05-13 20:35:02 +0800

    the first version, with low accuracy and overfitted.

--- main.py
+++ main.py
@@ -6,35 +6,41 @@ import torchvision
 from torchvision import datasets, models, transforms
 import os
 
-# 数据预处理步骤
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
 
-# 数据集
-data_dir = 'path_to_your_dataset'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                              shuffle=True, num_workers=4)
-               for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+def preProcess():
+    # 数据预处理步骤
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
 
-# 设备（CPU或GPU）
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # 数据集
+    # 获取当前文件路径
+    data_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'mid_hw_emotion_recognition')
+    print(data_dir)
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                              data_transforms[x])
+                      for x in ['train', 'test']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                  shuffle=True, num_workers=4)
+                   for x in ['train', 'test']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'test']}
+    class_names = image_datasets['train'].classes
+
+    # 设备（CPU或GPU）
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    return dataloaders, dataset_sizes, device
 
 
 # 构建模型
@@ -58,40 +64,50 @@ class FaceNet(nn.Module):
         return x
 
 
-model = FaceNet()
+def main():
+    # 数据预处理
+    dataloaders, dataset_sizes, device = preProcess()
 
-# 设置loss function 和 optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    model = FaceNet()
 
-# 训练模型
-for epoch in range(10):  # 迭代次数可更改
-    for phase in ['train', 'val']:
-        if phase == 'train':
-            model.train()
-        else:
-            model.eval()
 
-        running_loss = 0.0
-        running_corrects = 0
+    # 设置loss function 和 optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    model = model.to(device)
 
-        for inputs, labels in dataloaders[phase]:
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-
+    # 训练模型
+    for epoch in range(10):  # 迭代次数可更改
+        for phase in ['train', 'test']:
             if phase == 'train':
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+                model.train()
+            else:
+                model.eval()
 
-            _, preds = torch.max(outputs, 1)
-            running_loss += loss.item() * inputs.size(0)
-            running_corrects += torch.sum(preds == labels.data)
+            running_loss = 0.0
+            running_corrects = 0
 
-        epoch_loss = running_loss / dataset_sizes[phase]
-        epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-        print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+
+if __name__ == '__main__':
+    main()

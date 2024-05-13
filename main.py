e0804c8d1b05bf9d520658c8744e1575bfe18e4b
najxhodeyjvuzi <najxhodeyjvuzi@proton.me>
2024-05-13 17:22:09 +0800

    enable cuda and pytorch

--- main.py
+++ main.py
@@ -1,4 +1,97 @@
 import torch
-x = torch.rand(3,5)
-print(x)
-print(torch.cuda.is_available())
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import datasets, models, transforms
+import os
+
+# 数据预处理步骤
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+# 数据集
+data_dir = 'path_to_your_dataset'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                              shuffle=True, num_workers=4)
+               for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+# 设备（CPU或GPU）
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+# 构建模型
+class FaceNet(nn.Module):
+    def __init__(self):
+        super(FaceNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(256 * 28 * 28, 5)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = self.pool(nn.functional.relu(self.conv3(x)))
+        x = x.view(-1, 256 * 28 * 28)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        return x
+
+
+model = FaceNet()
+
+# 设置loss function 和 optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# 训练模型
+for epoch in range(10):  # 迭代次数可更改
+    for phase in ['train', 'val']:
+        if phase == 'train':
+            model.train()
+        else:
+            model.eval()
+
+        running_loss = 0.0
+        running_corrects = 0
+
+        for inputs, labels in dataloaders[phase]:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            if phase == 'train':
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            _, preds = torch.max(outputs, 1)
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+        epoch_loss = running_loss / dataset_sizes[phase]
+        epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+        print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
